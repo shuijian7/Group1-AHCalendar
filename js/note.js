4b8691422b3386df2493f945a6287a250f21b817
brycedigeronimo <brycedigeronimo@gmail.com>
2019-01-27 16:03:19 -0800

    adding functionality to get id from form

--- js/note.js
+++ js/note.js
@@ -1,6 +1,7 @@
 function dayClicked(e){
-
-    console.log(e.dataset.uid);
+    document.getElementById('id').value = e.dataset.uid;
+    console.log(document.getElementById('id').value);
+    console.log("hi");
     data.post_its.current_post_it_id = e.dataset.uid;
     currentDayHasNote(data.post_its.current_post_it_id);
     openModal(2);
