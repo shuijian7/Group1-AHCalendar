859150c16e6a0631c8da13290763208730e07348
Haojun Dong <jarvis@Haojuns-MacBook-Pro.local>
2019-02-05 16:26:15 -0800

    commenting on testing code(header), inline to do

--- test/data.test.js
+++ test/data.test.js
@@ -1,9 +1,12 @@
 /**
- * *********************************************
- * Testing for for Jest to test file data.js   *
- * CIS422 Winter 2019 Group1                   *
- * Author: Jarvis Dong, Micheal Zhang          *
- * *********************************************
+ * ********************************************
+ * Testing for for Jest to test file data.js   
+ * CIS422 Winter 2019 Group1                   
+ * Author: Jarvis Dong, Micheal Zhang          
+ * Last Updated: Feb 5th 2019
+ * For more information: go here 
+ * https://github.com/shuijian7/Group1-Calendar/blob/master/Documents/Testing%20Documentation.pdf              
+ * ********************************************
  */
 
 const month_data = require('../js/data');
