1c4c4d8d432ee85288a12ccb7e9f39f792ffd212
shuijian <32333924+shuijian@users.noreply.github.com>
2019-02-05 16:19:57 -0800

    PHP Test temporary

---
+++ tests/StackTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\tests;
+require_once __DIR__ . '/../vendor/autoload.php';
+define("ROOT_PATH", dirname(__DIR__) . "/");
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+use PHPUnit\Framework\TestCase;
+
+
+class StackTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+
+        // 添加日志文件,如果没有安装monolog，则有关monolog的代码都可以注释掉
+        $this->Log()->error('hello', $stack);
+
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+
+    public function Log()
+    {
+        // create a log channel
+        $log = new Logger('Tester');
+        $log->pushHandler(new StreamHandler(ROOT_PATH . 'storage/logs/app.log', Logger::WARNING));
+        $log->error("Error");
+        return $log;
+    }
+}
